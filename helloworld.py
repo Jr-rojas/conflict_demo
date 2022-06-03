b0900beb944610c0060ac6da23d32caa26bc1330
Jersain Rojas <j.rojas_7@hotmail.com>
2022-06-02 17:18:10 -0700

    merge

--- helloworld.py
+++ helloworld.py
@@ -1,18 +1,9 @@
-<<<<<<< HEAD
 def hello_world(city, state):
     print(f'Hello World! {city}, {state}!')
 
 
 def main():
     hello_world('NYC', 'New York')
-=======
-def hello_world(city):
-    print(f'Hello World! {city}!')
-
-
-def main():
-    hello_world('NYC')
->>>>>>> f5f1b1aade43aa88fc2ed5b22e66e26fe814f95a
 
 
 main()
